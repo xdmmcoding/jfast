1130a8a678894dc5739966f330905ccc0ad6ddd2
lixudongmm <484398643@qq.com>
2020-07-28 16:39:50 +0800

    修改Sharding分库分表自定义规则算法

--- src/main/java/com/jfast/algorithm/ActionlogShardingAlgorithm.java
+++ src/main/java/com/jfast/algorithm/ActionlogShardingAlgorithm.java
@@ -6,15 +6,13 @@ import java.util.Date;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
 
-import com.jfast.util.DateUtil;
-
 public class ActionlogShardingAlgorithm implements PreciseShardingAlgorithm<Date> {
 
 	@Override
 	public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Date> shardingValue) {
-		Date posttime = (Date) shardingValue.getValue();
-        String index = DateUtil.formatDate(posttime, "yyyyMMdd");
-        String tableReal = shardingValue.getLogicTableName().concat("_").concat(index);
-        return tableReal;
+//		Date posttime = (Date) shardingValue.getValue();
+//        String index = DateUtil.formatDate(posttime, "yyyyMMdd");
+//        String tableReal = shardingValue.getLogicTableName().concat("_").concat(index);
+        return shardingValue.getLogicTableName();
 	}
 }
